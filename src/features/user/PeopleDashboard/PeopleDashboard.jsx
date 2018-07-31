60b75fea338185270b26c33bb15befdb42dd7dd4
Ashton <ashtonberry@gmail.com>
2018-07-31 23:57:38 +0100

    Completed Section 6

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeopleDashboardPage = () => {
+  return (
+    <div>
+      <h1>People Dashboard Page </h1>
+    </div>
+  )
+}
+
+export default PeopleDashboardPage
